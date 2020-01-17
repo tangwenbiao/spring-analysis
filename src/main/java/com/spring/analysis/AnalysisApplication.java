7480fbab7db85ea9033009d61f3e5ed167a470d3
tangwenbiao <tangwenbiao@sm.vvip-u.com>
2020-01-17 15:26:32 +0800

    练习结构搭建完毕

---
+++ src/main/java/com/spring/analysis/AnalysisApplication.java
@@ -0,0 +1,17 @@
+package com.spring.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: TangFenQi
+ * @description:
+ * @date：2020/1/17 15:19
+ */
+@SpringBootApplication
+public class AnalysisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AnalysisApplication.class);
+  }
+}
